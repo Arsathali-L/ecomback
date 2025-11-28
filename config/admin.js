f0cdd605a3f55b29f98aa2b0d6f52cc50ff21280
L. Arsathali <arsathliakath092003@gmail.com>
2025-11-28 19:01:09 +0530

    ecomback

--- config/admin.js
+++ config/admin.js
@@ -2,5 +2,8 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_SECRET'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
   url: env('PUBLIC_URL') + '/admin',
 });
--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  
+
+  url: env('PUBLIC_URL'),   // 👈 REQUIRED FOR DEPLOYMENT !!
+
   app: {
     keys: env.array('APP_KEYS'),
   },
