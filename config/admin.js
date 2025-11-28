d2df689663b1d3baf487680ed61d2b5922fb5b0a
L. Arsathali <arsathliakath092003@gmail.com>
2025-11-28 17:24:37 +0530

    ecomback

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_SECRET'),
+    secret: env('AUTH_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
   },
   url: env('ADMIN_URL'),
 });
