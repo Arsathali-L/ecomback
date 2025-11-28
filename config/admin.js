7c36553f6170a2b37b65e307d82839ec9f119857
L. Arsathali <arsathliakath092003@gmail.com>
2025-11-28 12:55:58 +0530

    ecomback

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,24 @@
+// module.exports = ({ env }) => ({
+//   auth: {
+//     secret: env('ADMIN_JWT_SECRET'),
+//   },
+//   apiToken: {
+//     salt: env('API_TOKEN_SALT'),
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+
+  admin: {
+    url: env('ADMIN_URL', '/admin'),
+    auth: {
+      secret: env('ADMIN_JWT_SECRET')
+    },
   },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
+
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
